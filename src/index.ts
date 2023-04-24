7f9be8b74855569d6ab54b7398bfca6d752b84bd
luciano <lucianorosende@gmail.com>
2023-04-23 23:15:18 -0300

    types

--- src/index.ts
+++ src/index.ts
@@ -12,35 +12,44 @@ class ProductManager {
         this.products = products;
     }
     async getProducts(): Promise<Product[]> {
-        let res = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+        let res: Array<Product> = [];
+        try {
+            res = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+        } catch (e: any) {
+            console.log(e);
+        }
         return res;
     }
     async addProduct(prod: IProducts): Promise<boolean> {
-        let result = true;
-        if (this.products.length === 0) {
-            prod.id = 1;
-        } else {
-            let newId = this.products[this.products.length - 1]?.id ?? 0;
-            prod.id = newId + 1;
-        }
-
+        let result: boolean = true;
+        prod.id = this.products.length;
         if (this.products.find((obj) => obj?.title === prod.title)) {
             result = false;
         } else {
             this.products.push(prod);
-            await fs.promises.writeFile(
-                this.path,
-                JSON.stringify(this.products, null, 2)
-            );
+            try {
+                await fs.promises.writeFile(
+                    this.path,
+                    JSON.stringify(this.products, null, 2)
+                );
+            } catch (e: any) {
+                console.log(e);
+            }
         }
         return result;
     }
     async getProductById(id: number): Promise<Product> {
-        let res = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+        let res;
+        try {
+            res = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+        } catch (e: any) {
+            console.log(e);
+        }
         const findId = res.find((obj: Product) => obj?.id === id);
         return findId;
     }
-    async updateProduct(id: number, field: Product): Promise<object> {
+    async updateProduct(id: number, field: Product): Promise<Product> {
+        let file;
         let products = await this.getProducts();
         const newArr = products.map((obj) => {
             if (obj?.id === id) {
@@ -56,14 +65,16 @@ class ProductManager {
             }
             return obj;
         });
-        let updateFile = await fs.promises.writeFile(
-            this.path,
-            JSON.stringify(newArr, null, 2)
-        );
-        let readUpdatedFile = JSON.parse(
-            await fs.promises.readFile(this.path, "utf-8")
-        );
-        return readUpdatedFile;
+        try {
+            await fs.promises.writeFile(
+                this.path,
+                JSON.stringify(newArr, null, 2)
+            );
+            file = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+        } catch (e: any) {
+            console.log(e);
+        }
+        return file;
     }
 }
 
