97bb08a9cffecf44eb58bc365b5218befb16e63d
luciano <lucianorosende@gmail.com>
2023-05-05 08:46:22 -0300

    code refactoring

--- src/server.ts
+++ src/server.ts
@@ -16,12 +16,24 @@ app.get("/products/", async (req: Request, res: Response) => {
     let readProducts = await newProduct.getProducts();
     let numLimit = Number(limit);
     let newArr = readProducts.slice(0, numLimit);
-    if (numLimit < readProducts.length) {
-        res.status(200).json({ status: "success", data: newArr });
+    if (numLimit <= readProducts.length) {
+        res.status(200).json({
+            status: "success",
+            msg: `This is the list of products with the limit of ${numLimit}`,
+            data: newArr,
+        });
     } else if (numLimit > readProducts.length) {
-        res.status(400).json({ status: "error", msg: "limit exceeded" });
+        res.status(400).json({
+            status: "error",
+            msg: "limit exceeded",
+            data: {},
+        });
     } else {
-        res.status(200).json({ status: "success", data: readProducts });
+        res.status(200).json({
+            status: "success",
+            msg: "List of products",
+            data: readProducts,
+        });
     }
 });
 
@@ -29,14 +41,22 @@ app.get("/products/:pid", async (req: Request, res: Response) => {
     const { pid } = req.params;
     let getProductsID = await newProduct.getProductById(Number(pid));
     if (!getProductsID) {
-        res.status(404).json({ status: "error", msg: "404 product not found" });
+        res.status(404).json({
+            status: "error",
+            msg: "404 product not found",
+            data: {},
+        });
     } else {
-        res.status(200).json({ status: "success", data: getProductsID });
+        res.status(200).json({
+            status: "success",
+            msg: `This is the product with id: ${pid}`,
+            data: getProductsID,
+        });
     }
 });
 
 app.on("error", (err) => console.log("server error: " + err));
 
 app.get("*", (req: Request, res: Response) => {
-    res.status(404).json({ status: "error", msg: "route not found" });
+    res.status(404).json({ status: "error", msg: "route not found", data: {} });
 });
