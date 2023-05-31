dc812d7459d19d341e5164843dbc1ca11535f6de
luciano <lucianorosende@gmail.com>
2023-05-31 14:52:26 -0300

    added security

--- Server/src/utils/connectToMongoDB.ts
+++ Server/src/utils/connectToMongoDB.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export async function connectToMongoDB() {
     try {
         // Connect to MongoDB
-        await mongoose.connect(
-            "mongodb+srv://coderhouse:GKgNLrpzhKz9dYr3@proyect-ch.zgwmzwc.mongodb.net/?retryWrites=true&w=majority"
-        );
+        let url: string = process.env.MONGO_URL!;
+        await mongoose.connect(url);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
