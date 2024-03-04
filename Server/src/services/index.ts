9dfb7e10e2b5dda547f257867fd84b89272e46de
Luciano <lucianorosende@gmail.com>
2024-03-03 23:47:28 -0300

    react added

--- Server/src/services/index.ts
+++ Server/src/services/index.ts
@@ -1,6 +1,7 @@
 export * from "./carts.services";
 export * from "./messages.services";
 export * from "./products.services";
+export * from "./session.services";
 export * from "./ticket.services";
 export * from "./users.services";
 export * from "./views.services";
