f323771910d46dd2d819b3d64aaf28f64324f222
Luciano <lucianorosende@gmail.com>
2024-04-16 22:28:22 -0300

    popup added below search bar

--- Client/src/components/NavBar/SearchField.tsx
+++ Client/src/components/NavBar/SearchField.tsx
@@ -2,7 +2,12 @@ import SearchIcon from "@mui/icons-material/Search";
 import { styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Popper from "@mui/material/Popper";
+import Fade from "@mui/material/Fade";
 import { useState } from "react";
+import { ListItem, List } from "@mui/material";
+import { useFetchProducts } from "../../hooks";
 
 const Search = styled("div")(({ theme }) => ({
     position: "relative",
@@ -30,15 +35,12 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     color: "inherit",
     width: "100%",
     "& .MuiInputBase-input": {
-        padding: theme.spacing(1, 0, 0, 0),
+        padding: theme.spacing(1),
         // vertical padding + font size from searchIcon
         paddingLeft: `calc(1em + ${theme.spacing(3)})`,
         transition: theme.transitions.create("width"),
         [theme.breakpoints.up("sm")]: {
-            width: "12ch",
-            "&:focus": {
-                width: "20ch",
-            },
+            width: "18ch",
         },
     },
 }));
@@ -50,20 +52,54 @@ function SearchProductParameters(e, navigate) {
 
 export function SearchField() {
     const navigate = useNavigate();
+    const { productList } = useFetchProducts();
+    const [open, setOpen] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+        setOpen((previousOpen) => !previousOpen);
+    };
+
+    const canBeOpen = open && Boolean(anchorEl);
+    const id = canBeOpen ? "transition-popper" : undefined;
+
     return (
-        <Search>
-            <SearchIconWrapper>
-                <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                        SearchProductParameters(e, navigate);
-                    }
-                }}
-            />
-        </Search>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <Search>
+                <SearchIconWrapper>
+                    <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ "aria-label": "search" }}
+                    onClick={handleClick}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            SearchProductParameters(e, navigate);
+                        }
+                    }}
+                />
+            </Search>
+            <Popper id={id} open={open} anchorEl={anchorEl} transition>
+                {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={350}>
+                        <Box
+                            sx={{
+                                border: 1,
+                                p: 1,
+                                bgcolor: "background.paper",
+                            }}
+                        >
+                            <List>
+                                {productList.map((product) => {
+                                    return <ListItem>{product.title}</ListItem>;
+                                })}
+                            </List>
+                        </Box>
+                    </Fade>
+                )}
+            </Popper>
+        </div>
     );
 }
