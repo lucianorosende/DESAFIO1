355409ef5cf8fa5c752e3c00f45a0661cd002772
Luciano <lucianorosende@gmail.com>
2024-06-05 20:35:39 -0300

    test

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -43,7 +43,20 @@ export const httpServer = app.listen(PORT, () => {
 // Connecting Database --------------------------------------------------------------------------------------------------------------
 MongoDB();
 // Saving Sessions ------------------------------------------------------------------------------------------------------------------
-MongoSessions();
+// MongoSessions();
+app.set("trust proxy", 1);
+const sessionOptions = {
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URL,
+    }),
+    secret: "SECRET-CODE",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    proxy: true,
+    cookie: { httpOnly: false },
+};
+app.use(session(sessionOptions));
 
 // Initializing Passport ------------------------------------------------------------------------------------------------------------
 passportConfig();
