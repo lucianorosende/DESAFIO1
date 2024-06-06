cd080b924462f00905d4da8f56f3bdc7544816b2
Luciano <lucianorosende@gmail.com>
2024-06-06 05:43:58 -0300

    test

--- Server/src/middlewares/validateUser.ts
+++ Server/src/middlewares/validateUser.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import passport from "passport";
 
 export function validateUser(req: Request, res: Response, next: NextFunction) {
+    console.log(req.session);
     if ((req.session as any).passport?.user?.email) {
         return next();
     }
