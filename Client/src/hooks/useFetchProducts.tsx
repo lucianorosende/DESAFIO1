415bba440f5c2f2bf53b1aef95d66cacf399036f
Luciano <lucianorosende@gmail.com>
2024-06-05 21:15:06 -0300

    test

--- Client/src/hooks/useFetchProducts.tsx
+++ Client/src/hooks/useFetchProducts.tsx
@@ -7,7 +7,7 @@ export function useFetchProducts(title: string | undefined, limit: number) {
     useEffect(() => {
         const fetchData = async () => {
             const data = await fetch(
-                `${process.env.FETCH_UR}/api/products?pages=${renderPage}&title=${title}&limit=${limit}`,
+                `${process.env.FETCH_URL}/api/products?pages=${renderPage}&title=${title}&limit=${limit}`,
                 {
                     method: "GET",
                     headers: {
